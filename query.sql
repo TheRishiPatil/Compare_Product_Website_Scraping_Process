f5d17be255438b42e5ee28176ca7afd5b811d5cd
Rishikesh Krishna Patil <109085029+TheRishiPatil@users.noreply.github.com>
2023-09-02 23:17:16 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,6 @@
+use productData;
+select * from product_info;
+select * from product_category;
+
+-- tv, laptop, mobile, earphone, headphone, monitor, mouse, ac, refrigerator,  washing machine, pc, ps5 and games, router, filter, powerbank, inverter, tablets, watch
+-- gaming pc --> mouse, monitor, gpu, cpu, Cooling System, ram, Motherboard, keyboard, gaming chair, gaming pc cabinet
